cf1a054ab7904ea8316caf96840680344af4c58d
Arthurisme <is@live.ca>
2017-12-25 19:13:48 -0500

    spring add Oauth2

---
+++ OauthSso/src/main/java/com/intactai/OauthSsoApplication.java
@@ -0,0 +1,12 @@
+package com.intactai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSsoApplication.class, args);
+	}
+}
